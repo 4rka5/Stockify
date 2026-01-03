6772a6d35fc815308acea1e895fd63890e836c7c
4rka5 <4rka5@github.com>
2026-01-03 15:48:42 +0700

    repair

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -13,13 +13,17 @@ class CategoryRepository extends BaseRepository
 
     public function getAllWithProductCount()
     {
-        return $this->model->withCount('products')->get();
+        return $this->model->withCount(['products' => function ($query) {
+            $query->where('status', 'approved');
+        }])->get();
     }
 
     public function findByName($name)
     {
         return $this->model->where('name', 'like', '%' . $name . '%')
-            ->withCount('products')
+            ->withCount(['products' => function ($query) {
+                $query->where('status', 'approved');
+            }])
             ->get();
     }
 }
